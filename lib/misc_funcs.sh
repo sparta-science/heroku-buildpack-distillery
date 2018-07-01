ae52a60a63543c7b1bda14b4b7735d2f30b2b572
Greg Woodward <gregwoodward@gmail.com>
2018-07-01 08:15:25 -0700

    Try moving aws stuff into their own functions.

--- lib/misc_funcs.sh
+++ lib/misc_funcs.sh
@@ -10,3 +10,50 @@ export_env_dir() {
     done
   fi
 }
+
+download_aws_cli() {
+  AWS_CLI_URL="https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
+
+  # parse and derive params
+  BUILD_DIR=$1
+  CACHE_DIR=$2
+  BUILDPACK_DIR=$3
+
+  echo "-----> Fetching AWS CLI into slug"
+  curl --progress-bar -o /tmp/awscli-bundle.zip $AWS_CLI_URL
+  unzip -qq -d "$BUILD_DIR/vendor" /tmp/awscli-bundle.zip
+
+#  echo "-----> adding installer script into app/.profile.d"
+#  mkdir -p $BUILD_DIR/.profile.d
+#  cp "$BUILDPACK_DIR/bin/install_awscli.sh" $BUILD_DIR/.profile.d/
+#  chmod +x $BUILD_DIR/.profile.d/install_awscli.sh
+
+  AWS_INSTALL_DIR=$BUILD_DIR/vendor/awscli
+  chmod +x $BUILD_DIR/vendor/awscli-bundle/install
+  $BUILD_DIR/vendor/awscli-bundle/install -i $AWS_INSTALL_DIR
+  chmod u+x $AWS_INSTALL_DIR/bin/aws
+
+  export aws=$AWS_INSTALL_DIR/bin/aws
+
+  #cleaning up...
+  rm -rf /tmp/awscli*
+
+  echo "-----> aws cli installation done"
+}
+
+create_aws_credentials() {
+  mkdir ~/.aws
+
+  cat >> ~/.aws/credentials << EOF
+  [default]
+  aws_access_key_id = $AWS_KEY
+  aws_secret_access_key = $AWS_SECRET_KEY
+  EOF
+
+  cat >> ~/.aws/config << EOF
+  [default]
+  region = $AWS_REGION
+  EOF
+  echo "-----> AWS credentials created"
+
+}
