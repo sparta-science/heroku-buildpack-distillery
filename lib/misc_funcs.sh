d69551308c6525d1535d52327accf235c470e492
Greg Woodward <gregwoodward@gmail.com>
2018-07-01 11:53:25 -0700

    Variable cleanup

--- lib/misc_funcs.sh
+++ lib/misc_funcs.sh
@@ -12,24 +12,17 @@ export_env_dir() {
 }
 
 download_aws_cli() {
-  BUILD_DIR=$1
-  CACHE_DIR=$2
-  BUILDPACK_DIR=$3
+  build_dir=$1
 
+  AWS_INSTALL_DIR=$build_dir/vendor/awscli
   AWS_CLI_URL="https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
 
   echo "-----> Fetching AWS CLI into slug"
   curl --progress-bar -o /tmp/awscli-bundle.zip $AWS_CLI_URL
-  unzip -qq -d "$BUILD_DIR/vendor" /tmp/awscli-bundle.zip
+  unzip -qq -d "$build_dir/vendor" /tmp/awscli-bundle.zip
 
-#  echo "-----> adding installer script into app/.profile.d"
-#  mkdir -p $BUILD_DIR/.profile.d
-#  cp "$BUILDPACK_DIR/bin/install_awscli.sh" $BUILD_DIR/.profile.d/
-#  chmod +x $BUILD_DIR/.profile.d/install_awscli.sh
-
-  AWS_INSTALL_DIR=$BUILD_DIR/vendor/awscli
-  chmod +x $BUILD_DIR/vendor/awscli-bundle/install
-  $BUILD_DIR/vendor/awscli-bundle/install -i $AWS_INSTALL_DIR
+  chmod +x $build_dir/vendor/awscli-bundle/install
+  $build_dir/vendor/awscli-bundle/install -i $AWS_INSTALL_DIR
   chmod u+x $AWS_INSTALL_DIR/bin/aws
 
   export aws=$AWS_INSTALL_DIR/bin/aws
@@ -41,17 +34,21 @@ download_aws_cli() {
 }
 
 create_aws_credentials() {
+  aws_key=$1
+  aws_secret_key=$2
+  aws_region=$3
+
   mkdir ~/.aws
 
   cat >> ~/.aws/credentials << EOF
 [default]
-aws_access_key_id = $AWS_KEY
-aws_secret_access_key = $AWS_SECRET_KEY
+aws_access_key_id = $aws_key
+aws_secret_access_key = $aws_secret_key
 EOF
 
   cat >> ~/.aws/config << EOF
 [default]
-region = $AWS_REGION
+region = $aws_region
 EOF
   echo "-----> AWS credentials created"
 
