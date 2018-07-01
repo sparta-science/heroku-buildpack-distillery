f94078a6da9ec4e9a124f358bff718dc6decfe08
Greg Woodward <gregwoodward@gmail.com>
2018-07-01 08:20:26 -0700

    Try moving the EOF to first of line.

--- lib/misc_funcs.sh
+++ lib/misc_funcs.sh
@@ -45,15 +45,15 @@ create_aws_credentials() {
   mkdir ~/.aws
 
   cat >> ~/.aws/credentials << EOF
-  [default]
-  aws_access_key_id = $AWS_KEY
-  aws_secret_access_key = $AWS_SECRET_KEY
-  EOF
+[default]
+aws_access_key_id = $AWS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+EOF
 
   cat >> ~/.aws/config << EOF
-  [default]
-  region = $AWS_REGION
-  EOF
+[default]
+region = $AWS_REGION
+EOF
   echo "-----> AWS credentials created"
 
 }
